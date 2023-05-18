a1949a2a4a2120387c317ca5d4fac61c23e5e2fe
itssabhishek <itsabhishek0710@gmail.com>
2023-05-18 22:53:46 +0530

    Scrapping links from Google

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,38 @@
 'use client';
-import {useEffect, useRef, useState} from 'react';
-import {scrapeWebsite} from "@/utils/scrapper";
+import React, { useRef, } from 'react';
+import { scrapeWebsite } from '@/utils/scrapper';
 
 export default function Home() {
-const inputRef = useRef();
-const [url,setUrl] = useState('https://www.udemy.com/')
-  useEffect(() => {
-    const handleScrape = async () => {
-      const data = await scrapeWebsite(url);
-      console.log(data);
-    };
-
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const handleScrape = async () => {
+    const links = await scrapeWebsite(inputRef.current.value);
+    console.log(links);
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     handleScrape();
-  }, [url]);
+  };
 
-  return <form onSubmit={()=> setUrl(inputRef.current.value)}>
-    <label>Enter your website:</label>
-    <input type={"text"} ref={inputRef}/>
-    <button type={'submit'}>Scrap</button>
-  </form>;
+  return (
+      <div className="w-3/5 mx-auto ">
+        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="query">
+              Query
+            </label>
+            <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="query" type="text" placeholder="Query" ref={inputRef}/>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit">
+        Scrape
+            </button>
+          </div>
+        </form>
+      </div>
+  );
 }
--- utils/scrapper.ts
+++ utils/scrapper.ts
@@ -1,17 +1,27 @@
 import * as cheerio from 'cheerio';
 
-export async function scrapeWebsite(url: string) {
+export async function scrapeWebsite(query: string) {
   try {
-    const response = await fetch(url);
+    const response = await fetch(`https://www.google.com/search?q=${query}+course`);
     const html = await response.text();
     const $ = cheerio.load(html);
+    const topLinks = $('.yuRUbf a');
+    const links: string[] = [];
+    let count:number = 0;
 
-    // Use cheerio to extract data from the HTML
-    // Example:
-    const title = $('body');
+    topLinks.each((_, element) => {
+      if (count < 5) {
+        const value = $(element).attr('href');
+        if (value) {
+          links.push(value);
+        }
+        count++;
+      }
+    });
 
-    return { title };
+    return links;
   } catch (error) {
     console.error('Error scraping website:', error);
   }
 }
+
